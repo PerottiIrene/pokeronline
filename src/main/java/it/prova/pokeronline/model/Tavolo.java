ae803f4fcde3e6fbf8bb5fb61680cf831ddf5c57
PerottiIrene <ireneperotti29@gmail.com>
2022-11-29 11:32:10 +0100

    create classi di model

---
+++ src/main/java/it/prova/pokeronline/model/Tavolo.java
@@ -0,0 +1,115 @@
+package it.prova.pokeronline.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tavolo")
+public class Tavolo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "esperienzaMinima")
+	private Integer esperienzaMinima;
+	@Column(name = "cifraMinima")
+	private Integer cifraMinima;
+	@Column(name = "denominazione")
+	private String denominazione;
+	@Column(name = "dataCreazione")
+	private Date dataCreazione;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tavolo")
+	private Set<Utente> utentiGiocatori = new HashSet<Utente>(0);
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "utente_id", nullable = false)
+	private Utente utenteCreazione;
+	
+	public Tavolo() {}
+
+	public Tavolo(Long id, Integer esperienzaMinima, Integer cifraMinima, String denominazione, Date dataCreazione,
+			Set<Utente> utentiGiocatori, Utente utenteCreazione) {
+		super();
+		this.id = id;
+		this.esperienzaMinima = esperienzaMinima;
+		this.cifraMinima = cifraMinima;
+		this.denominazione = denominazione;
+		this.dataCreazione = dataCreazione;
+		this.utentiGiocatori = utentiGiocatori;
+		this.utenteCreazione = utenteCreazione;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getEsperienzaMinima() {
+		return esperienzaMinima;
+	}
+
+	public void setEsperienzaMinima(Integer esperienzaMinima) {
+		this.esperienzaMinima = esperienzaMinima;
+	}
+
+	public Integer getCifraMinima() {
+		return cifraMinima;
+	}
+
+	public void setCifraMinima(Integer cifraMinima) {
+		this.cifraMinima = cifraMinima;
+	}
+
+	public String getDenominazione() {
+		return denominazione;
+	}
+
+	public void setDenominazione(String denominazione) {
+		this.denominazione = denominazione;
+	}
+
+	public Date getDataCreazione() {
+		return dataCreazione;
+	}
+
+	public void setDataCreazione(Date dataCreazione) {
+		this.dataCreazione = dataCreazione;
+	}
+
+	public Set<Utente> getUtentiGiocatori() {
+		return utentiGiocatori;
+	}
+
+	public void setUtentiGiocatori(Set<Utente> utentiGiocatori) {
+		this.utentiGiocatori = utentiGiocatori;
+	}
+
+	public Utente getUtenteCreazione() {
+		return utenteCreazione;
+	}
+
+	public void setUtenteCreazione(Utente utenteCreazione) {
+		this.utenteCreazione = utenteCreazione;
+	}
+	
+	
+
+}
